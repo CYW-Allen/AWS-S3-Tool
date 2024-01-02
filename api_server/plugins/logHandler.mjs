0faabfe3b7f4b51174f97d29bfc8e33fa2e05f06
Allen <allen.chen@happytomato.com.tw>
2024-01-02 15:24:13 +0800

    [api_server] implement log handler

---
+++ api_server/plugins/logHandler.mjs
@@ -0,0 +1,14 @@
+import fastifyPlugin from "fastify-plugin";
+import log from 'npmlog';
+import dayjs from 'dayjs';
+
+log.prefixStyle.bold = true;
+log.prefixStyle.fg = 'blue';
+log.headingStyle.fg = 'yellow';
+
+export default fastifyPlugin(async function (fastify, opts) {
+  fastify.decorate('logStat', function (type, caller, content) {
+    log.heading = dayjs().format('YYYY/MM/DD HH:mm:ss');
+    log[type](`[${caller}]`, content);
+  });
+}, { name: 'logHandler' });
