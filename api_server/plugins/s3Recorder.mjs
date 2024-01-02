de8c82604ce7e5e2cf1df3ebe3c7d1aa3ad8fb65
Allen <allen.chen@happytomato.com.tw>
2024-01-02 15:59:18 +0800

    [api_server] feat: implement object operation history management

---
+++ api_server/plugins/s3Recorder.mjs
@@ -0,0 +1,257 @@
+import fastifyPlugin from "fastify-plugin";
+import { randomUUID } from 'crypto';
+import dayjs from "dayjs";
+import { BatchWriteCommand, GetCommand, PutCommand, QueryCommand, ScanCommand } from "@aws-sdk/lib-dynamodb";
+
+export default fastifyPlugin(async function (fastify, opts) {
+  function fillListWithItems(list, items) {
+    for (let i = 0; i < items.length; i++) list.push(items[i]);
+  }
+
+  function getDayList(begin, end) {
+    const sNum = Number(begin);
+    const eNum = Number(end || begin);
+    const st = dayjs(Math.min(sNum, eNum).toString());
+    const et = dayjs(Math.max(sNum, eNum).toString()).add(1, 'day');
+    const dayList = [];
+
+    for (let ct = st; ct.isBefore(et); ct = ct.add(1, 'day')) {
+      dayList.push(Number(ct.format('YYYYMMDD')));
+    }
+    return dayList;
+  }
+
+  function sanitizeFilters(mainObj, filters) {
+    const queryField = ['action', 'createdTimeNum', 'editor', 'objKey'];
+
+    delete filters[mainObj];
+    Object.keys(filters).forEach((key) => {
+      if (!queryField.includes(key)) delete filters[key];
+    });
+  }
+
+  function getQueryConfig(queryCondition) {
+    const { TableName, obj, objVal, filters } = queryCondition;
+    const expressionConfig = Object.entries(filters || {})
+      .reduce((result, [field, val]) => {
+        (result.FilterExpression ??= []).push(`#${field} = :v_${field}`);
+        (result.ExpressionAttributeNames ??= {})[`#${field}`] = field;
+        (result.ExpressionAttributeValues ??= {})[`:v_${field}`] = val;
+        return result;
+      }, {});
+
+    if (filters) {
+      expressionConfig.FilterExpression = expressionConfig.FilterExpression.join(' AND ');
+    }
+    (expressionConfig.ExpressionAttributeNames ??= {})[`#${obj}`] = obj;
+    (expressionConfig.ExpressionAttributeValues ??= {})[`:v_${obj}`] = objVal;
+
+    return {
+      TableName,
+      IndexName: `${obj}-index`,
+      KeyConditionExpression: `#${obj} = :v_${obj}`,
+      ...expressionConfig,
+    };
+  }
+
+  async function queryHistory(caller, queryConfig) {
+    try {
+      const dbCmd = new QueryCommand(queryConfig);
+      const recs = [];
+
+      do {
+        const { Items, LastEvaluatedKey } = await fastify.dbClient.send(dbCmd);
+
+        fillListWithItems(recs, Items || []);
+        dbCmd.input.ExclusiveStartKey = LastEvaluatedKey;
+      } while (dbCmd.input.ExclusiveStartKey);
+      return recs;
+    } catch (err) {
+      fastify.logStat('error', `${caller}->queryHistory`, err);
+      return [];
+    }
+  }
+
+  async function checkUserExist(username) {
+    const user = await fastify.dbClient.send(new GetCommand({
+      TableName: fastify.userTable,
+      Key: { username },
+    }));
+
+    return Boolean(user.Item);
+  }
+
+  async function getDelReqItems(TableName, queryCondition, caller) {
+    const { obj, objVal, filters } = queryCondition;
+    const reqItems = [];
+    const failure = [];
+
+    if (obj === 'createdTimeNum') {
+      const days = getDayList(...objVal.split('~'));
+      const results = (await Promise.allSettled(
+        days.map((day) => queryHistory(caller, getQueryConfig({ TableName, obj, objVal: day, filters })))
+      ));
+
+      results.forEach((result, index) => {
+        if (result.status === 'fulfilled') {
+          const delRequests = result.value.map((row) => ({
+            DeleteRequest: {
+              Key: { recId: row.recId },
+            },
+          }));
+
+          fillListWithItems(reqItems, delRequests);
+        } else failure.push(`Fail to get history in ${days[index]}`);
+      });
+    } else {
+      const delRequests = (await queryHistory(caller, getQueryConfig({ TableName, obj, objVal, filters })))
+        .map((row) => ({
+          DeleteRequest: {
+            Key: { recId: row.recId },
+          },
+        }));
+
+      fillListWithItems(reqItems, delRequests);
+    }
+
+    return { reqItems, failure };
+  }
+
+  fastify.decorate('recordOper', async function (bucket, objKey, action, editor) {
+    try {
+      await fastify.dbClient.send(new PutCommand({
+        TableName: fastify.historyTable,
+        Item: {
+          recId: randomUUID(),
+          bucket,
+          objKey,
+          createdAt: Date.now(),
+          createdTimeNum: Number(dayjs(new Date()).format('YYYYMMDD')),
+          action,
+          editor,
+        },
+      }));
+      fastify.logStat('info', `${editor}->recordOper`, `User ${editor} success to reocrd s3 operation`);
+    } catch (err) {
+      fastify.logStat('error', `${editor}->recordOper`, err);
+    }
+  });
+
+  fastify.decorate('listHistory', async function (req, reply) {
+    const caller = req.user.username;
+    const bucket = req.params.bucket;
+    const recs = [];
+
+    try {
+      const dbCmd = new ScanCommand({
+        TableName: fastify.historyTable,
+        FilterExpression: '#bucket = :v_bucket',
+        ExpressionAttributeNames: { '#bucket': 'bucket' },
+        ExpressionAttributeValues: { ':v_bucket': bucket },
+      });
+
+      do {
+        const { Items, LastEvaluatedKey } = await fastify.dbClient.send(dbCmd);
+
+        fillListWithItems(recs, Items);
+        dbCmd.input.ExclusiveStartKey = LastEvaluatedKey;
+      } while (dbCmd.input.ExclusiveStartKey);
+      return reply.send({ data: recs });
+    } catch (err) {
+      fastify.logStat('error', `${caller}->listHistory`, err);
+      return reply.code(500).send();
+    }
+  });
+
+  fastify.decorate('getHistoryByCondition', async function (req, reply) {
+    const { obj, objVal, filters } = req.body;
+    const TableName = fastify.historyTable;
+    const caller = req.user.username;
+
+    if (filters) sanitizeFilters(obj, filters);
+    if (obj === 'createdTimeNum') {
+      const days = getDayList(...objVal.split('~'));
+      const results = (await Promise.allSettled(days.map((day) => (
+        queryHistory(caller, getQueryConfig({ TableName, obj, objVal: day, filters }))
+      ))));
+
+      return reply.send({
+        data: results.reduce((output, result) => {
+          if (result.status === 'fulfilled') fillListWithItems(output, result.value);
+          return output;
+        }, []),
+      });
+    }
+    return reply.send({
+      data: await queryHistory(caller, getQueryConfig({ TableName, obj, objVal, filters })),
+    });
+  });
+
+  fastify.decorate('addHistory', async function (req, reply) {
+    const bucket = req.params.bucket;
+    const caller = req.user.username;
+    const { objKey, action, editor, createdTimeNum } = req.body;
+
+    try {
+      const editorIsExist = await checkUserExist(editor);
+
+      if (!editorIsExist) {
+        return reply.code(404).send({ message: 'This editor doesn\'t exist' });
+      }
+
+      await fastify.dbClient.send(new PutCommand({
+        TableName: fastify.historyTable,
+        Item: {
+          recId: randomUUID(),
+          bucket,
+          objKey,
+          createdAt: dayjs(createdTimeNum.toString()).unix(),
+          createdTimeNum: Number(createdTimeNum),
+          action,
+          editor,
+        },
+      }));
+      fastify.logStat('info', `${caller}->addHistory`,
+        `Success to add object history ([${createdTimeNum}] ${editor} ${action} ${objKey})`);
+      return reply.send({ data: null });
+    } catch (err) {
+      fastify.logStat('error', `${caller}->addHistory`, err);
+      return reply.code(500).send();
+    }
+  });
+
+  fastify.decorate('deleteHistory', async function (req, reply) {
+    const caller = req.user.username;
+    const historyTable = fastify.historyTable;
+    const queryCondition = {
+      ...req.body,
+      filters: { bucket: req.params.bucket },
+    }
+
+    try {
+      const { reqItems, failure } = await getDelReqItems(historyTable, queryCondition, caller)
+
+      if (reqItems.length) {
+        const { UnprocessedItems } = await fastify.dbClient.send(new BatchWriteCommand({
+          RequestItems: { [historyTable]: reqItems },
+        }));
+
+        if (UnprocessedItems[historyTable]) {
+          failure.push(`Fail to delete history (count: ${UnprocessedItems[historyTable].length})`);
+          UnprocessedItems[historyTable].forEach((item) => {
+            fastify.logStat('error', `${caller}->deleteHistory`, `Fail deleted history id: ${item.DeleteRequest.Key.recId}`);
+          });
+        } else fastify.logStat('info', `${caller}->deleteHistory`, 'Success to delete the history');
+      }
+
+      return reply.send({ failure });
+    } catch (err) {
+      fastify.logStat('error', `${caller}->deleteHistory`, err);
+      return reply.code(500).send();
+    }
+  });
+
+}, {
+  name: 's3Recorder',
+  dependencies: ['dbInitializer', 'logHandler'],
+});
--- api_server/routes/s3History/index.mjs
+++ api_server/routes/s3History/index.mjs
@@ -1,5 +1,118 @@
+import S from 'fluent-json-schema';
+
 export default async function (fastify, _opts) {
-  fastify.get('/', async function (req, reply) {
-    reply.send('s3History route');
-  });
+  const historyProps = ['action', 'createdTimeNum', 'editor', 'objKey'];
+  const s3Operations = ['create', 'upload', 'modify', 'switch', 'delete', 'restore'];
+
+  const respInvalidReq = fastify.clientErrResponse();
+  const respUnpermitReq = fastify.clientErrResponse(
+    S.object().description('Insufficient permissions'),
+  );
+
+  const optsListHistory = {
+    schema: {
+      summary: 'List all history',
+      description: 'List all s3 objects\' operations history in specific bucket',
+      tags: ['S3 history'],
+      security: [{ jwt: [] }],
+      params: S.object().prop('bucket', fastify.schemaNotEmpty.required()),
+      response: {
+        200: S.object()
+          .description('Success to get history of the bucket')
+          .prop('data', S.array().items(S.object().additionalProperties(true))),
+        400: respInvalidReq,
+        403: respUnpermitReq,
+        500: fastify.svrErrResponse,
+      },
+    },
+    onRequest: [fastify.verifyToken, fastify.checkPermission],
+  };
+
+  const optsGetHistoryByCondition = {
+    schema: {
+      summary: 'List specific history',
+      description: 'List history under the specific conditions',
+      tags: ['S3 history'],
+      security: [{ jwt: [] }],
+      params: S.object().prop('bucket', fastify.schemaNotEmpty.required()),
+      body: S.object()
+        .prop('obj', S.string().enum(historyProps).required())
+        .prop('filters', S.object()
+          .prop('action', S.string().enum(s3Operations))
+          .prop('createdTimeNum', fastify.schemaNotEmpty)
+          .prop('editor', fastify.schemaNotEmpty)
+          .prop('objKey', fastify.schemaNotEmpty)
+        )
+        .ifThenElse(
+          S.object().prop('obj', S.string().const('action')),
+          S.object().prop('objVal', S.string().enum(s3Operations).required()),
+          S.object().prop('objVal', fastify.schemaNotEmpty.required())
+        ),
+      response: {
+        200: S.object()
+          .description('Success to get specific history of the bucket')
+          .prop('data', S.array().items(S.object().additionalProperties(true))),
+        400: respInvalidReq,
+        403: respUnpermitReq,
+        500: fastify.svrErrResponse,
+      }
+    },
+    onRequest: [fastify.verifyToken, fastify.checkPermission],
+  };
+
+  const optsAddHistory = {
+    schema: {
+      summary: 'Add new history',
+      description: 'Manual add new history of the object',
+      tags: ['S3 history'],
+      security: [{ jwt: [] }],
+      params: S.object().prop('bucket', fastify.schemaNotEmpty.required()),
+      body: S.object()
+        .prop('objKey', fastify.schemaNotEmpty.required())
+        .prop('action', S.string().enum(s3Operations).required())
+        .prop('editor', fastify.schemaNotEmpty.required())
+        .prop('createdTimeNum', fastify.schemaNotEmpty.required()),
+      response: {
+        200: S.object()
+          .description('Success to add new object history')
+          .prop('data', S.null()),
+        400: respInvalidReq,
+        403: respUnpermitReq,
+        404: fastify.clientErrResponse(S.object().description('Invaild editor')),
+        500: fastify.svrErrResponse,
+      },
+    },
+    onRequest: [fastify.verifyToken, fastify.checkAdmin]
+  };
+
+  const optsDeleteHistory = {
+    schema: {
+      summary: 'Delete history',
+      description: 'Delete the history under the specific condition',
+      tags: ['S3 history'],
+      security: [{ jwt: [] }],
+      params: S.object().prop('bucket', fastify.schemaNotEmpty.required()),
+      body: S.object()
+        .prop('obj', S.string().enum(historyProps).required())
+        .ifThenElse(
+          S.object().prop('obj', S.string().const('action')),
+          S.object().prop('objVal', S.string().enum(s3Operations).required()),
+          S.object().prop('objVal', fastify.schemaNotEmpty.required())
+        ),
+      response: {
+        200: S.object()
+          .description('Finish to delete history')
+          .prop('failure', S.array().items(S.string())),
+        400: respInvalidReq,
+        403: respUnpermitReq,
+        500: fastify.svrErrResponse,
+      }
+    },
+    onRequest: [fastify.verifyToken, fastify.checkAdmin],
+  };
+
+  fastify.get('/:bucket', optsListHistory, fastify.listHistory);
+  fastify.post('/:bucket', optsGetHistoryByCondition, fastify.getHistoryByCondition);
+  fastify.put('/:bucket', optsAddHistory, fastify.addHistory);
+  fastify.delete('/:bucket', optsDeleteHistory, fastify.deleteHistory);
 };
