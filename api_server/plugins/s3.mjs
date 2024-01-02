13e74fed707dd30bc49c3b00bf26733a757e3362
Allen <allen.chen@happytomato.com.tw>
2024-01-02 16:10:34 +0800

    [api_server] feat: implement S3 object manipulations

---
+++ api_server/plugins/s3.mjs
@@ -0,0 +1,482 @@
+import fastifyPlugin from "fastify-plugin";
+import {
+  CopyObjectCommand,
+  DeleteObjectCommand,
+  DeleteObjectsCommand,
+  GetObjectCommand,
+  HeadObjectCommand,
+  ListBucketsCommand,
+  ListObjectVersionsCommand,
+  ListObjectsV2Command,
+  PutObjectCommand,
+  S3Client
+} from '@aws-sdk/client-s3';
+import { CloudFrontClient, CreateInvalidationCommand, ListDistributionsCommand } from '@aws-sdk/client-cloudfront';
+import { createPresignedPost } from '@aws-sdk/s3-presigned-post';
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
+import { lookup } from 'mime-types';
+import dayjs from 'dayjs';
+import localizedFormat from 'dayjs/plugin/localizedFormat.js';
+import 'dayjs/locale/zh-tw.js';
+
+export default fastifyPlugin(async function (fastify, opts) {
+  dayjs.extend(localizedFormat);
+  dayjs.locale('zh-tw');
+
+  const credObj = {
+    credentials: {
+      accessKeyId: process.env.AWS_ACCESS_KEY,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    },
+  };
+  const s3Client = new S3Client({
+    region: process.env.AWS_S3_REGION,
+    ...(process.env.NODE_ENV === 'dev' && credObj),
+  });
+  const cloudfrontClient = new CloudFrontClient({
+    region: process.env.AWS_CLOUDFRONT_REGION,
+    ...(process.env.NODE_ENV === 'dev' && credObj),
+  });
+
+  const RESERVED_KEY = process.env.RESERVED_KEY.split(',');
+
+  const getTimeStr = (time) => dayjs(time).format('LL(dd) HH:mm');
+
+  function getFileSizeString(fileSize) {
+    const fsNum = Number(fileSize);
+
+    if (isNaN(fsNum)) throw new Error('Invalid file size value');
+    if (fsNum < 1000) return '1KB';
+    if (fsNum < 1000000) return `${Math.round(fsNum * 0.001)}KB`;
+    return `${Math.round(fsNum * 0.000001)}MB`;
+  }
+
+  function structureObjs(objs) {
+    const objStructure = {};
+
+    if (objs instanceof Array) {
+      objs.forEach((obj) => {
+        const dirKeys = obj.Key.split('/').filter((k) => k !== '');
+
+        if (RESERVED_KEY.includes(dirKeys[0])) return;
+
+        dirKeys.forEach((key, index) => {
+          const isFile = (index === dirKeys.length - 1) && obj.Key[obj.Key.length - 1] !== '/';
+          const objDir = index === 0 ? '/' : `/${dirKeys.slice(0, index).join('/')}`;
+          const objPath = `${objDir === '/' ? '' : objDir}/${key}`;
+
+          (objStructure[objDir] ??= {})[objPath] = {
+            name: key,
+            isFile,
+            lastModified: getTimeStr(obj.LastModified),
+            lastTime: obj.LastModified.getTime(),
+            ...(isFile && {
+              size: obj.Size,
+              sizeStr: getFileSizeString(obj.Size),
+            }),
+          };
+        });
+      });
+    }
+    return objStructure;
+  }
+
+  async function listAllObjs(req) {
+    const Bucket = req.params.bucket;
+    const user = req.user.username;
+    const result = [];
+    let st = Date.now();
+    let times = 0;
+
+    try {
+      const s3Cmd = new ListObjectsV2Command({ Bucket });
+
+      do {
+        const resp = await s3Client.send(s3Cmd);
+
+        for (let i = 0; i < resp.Contents?.length || 0; i++) result.push(resp.Contents[i]);
+        s3Cmd.input.ContinuationToken = resp.NextContinuationToken;
+        times++;
+      } while (s3Cmd.input.ContinuationToken);
+
+      fastify.logStat('info', `${user}->listAllObjs`, `Time cost: ${Date.now() - st}ms; loops: ${times}`);
+      return { data: structureObjs(result) };
+    } catch (err) {
+      fastify.logStat('error', `${user}->listAllObjs`, `${err.name}: ${err.message}`);
+      if (err.name === 'NoSuchBucket') return undefined;
+      return null;
+    }
+  }
+
+  async function downloadFiles(req) {
+    const Bucket = req.params.bucket;
+    const reqList = req.query.reqList.split(',');
+    const fileUrls = [];
+    const failedKeys = [];
+    const results = await Promise.allSettled(reqList.map((Key) => getSignedUrl(
+      s3Client,
+      new GetObjectCommand({
+        Bucket,
+        Key,
+        ResponseContentDisposition: `attachment; filename=${encodeURI(Key.split('/').slice(-1)[0])}`,
+      }),
+      { expiresIn: 3000 },
+    )));
+
+    results.forEach((result, index) => {
+      if (result.status === 'fulfilled') fileUrls.push(result.value);
+      else {
+        const failKey = reqList[index];
+
+        failedKeys.push(failKey);
+        fastify.logStat('error', `${req.user.username}->downloadFiles`, `${Bucket}/${failKey} fail - ${result.reason}`);
+      }
+    });
+    return { data: { fileUrls, failedKeys } };
+  }
+
+  async function getObjVersion(req) {
+    try {
+      const objKey = req.query.objKey;
+      const s3Cmd = new ListObjectVersionsCommand({
+        Bucket: req.params.bucket,
+        Prefix: objKey,
+      });
+      const objVerInfos = [];
+
+      do {
+        const resp = await s3Client.send(s3Cmd);
+        const infos = resp.Versions
+          ?.filter((ver) => ver.Key === objKey && !ver.IsLatest)
+          ?.map((ver) => ({
+            createdAt: ver.LastModified,
+            size: getFileSizeString(ver.Size),
+            verId: ver.VersionId,
+          })) || [];
+
+        for (let i = 0; i < infos.length; i++) objVerInfos.push(infos[i]);
+        s3Cmd.input.VersionIdMarker = resp.NextVersionIdMarker;
+      } while (s3Cmd.input.VersionIdMarker);
+
+      return { data: { objVerInfos } };
+    } catch (err) {
+      fastify.logStat('error', `${req.user.username}->getObjVersion`, `${err.name}: ${err.message}`);
+      if (err.name === 'NoSuchBucket') return undefined;
+      return null;
+    }
+  }
+
+  async function createFolder(req) {
+    const Bucket = req.params.bucket;
+    const user = req.user.username;
+    const Key = req.body.objKeys[0];
+    const respData = {
+      data: {
+        fileUrls: [],
+        failedKeys: [],
+      },
+    };
+
+    try {
+      await s3Client.send(new HeadObjectCommand({ Bucket, Key }));
+      fastify.logStat('info', `${user}->createFolder`, `The folder (${Bucket}:${Key}) has already existed`);
+      return respData;
+    } catch (err) {
+      if (err.name === 'NotFound') {
+        try {
+          await s3Client.send(new PutObjectCommand({ Bucket, Key, ContentLength: 0 }));
+          fastify.logStat('info', `${user}->createFolder`, `${Bucket}:${Key} success`);
+          await fastify.recordOper(Bucket, Key, 'create', user);
+          return respData;
+        } catch (e) {
+          fastify.logStat('error', `${user}->createFolder`, `${e.name}: ${e.message}`);
+          if (e.name === 'NoSuchBucket') return undefined;
+          return null;
+        }
+      }
+      fastify.logStat('error', `${user}->createFolder`, `${err.name}: ${err.message}`);
+      return null;
+    }
+  }
+
+  async function getUploadUrls(req) {
+    const bucket = req.params.bucket;
+    const user = req.user.username;
+    const { objKeys, isPublic } = req.body;
+
+    const fileUrls = [];
+    const failedKeys = [];
+    const results = await Promise.allSettled(
+      objKeys?.map((Key) => createPresignedPost(
+        s3Client,
+        {
+          Bucket: bucket,
+          Key,
+          Conditions: [{ bucket }, ...(isPublic && [{ acl: 'public-read' }])],
+          Fields: {
+            ...(isPublic && { acl: 'public-read' }),
+            'Content-Type': lookup(Key) || 'application/octet-stream',
+          },
+          Expires: 3000,
+        },
+      )) || []
+    );
+
+    results.forEach((result, index) => {
+      if (result.status === 'fulfilled') fileUrls.push(result.value);
+      else {
+        const failKey = objKeys[index];
+        failedKeys.push(failKey);
+        fastify.logStat('error', `${user}->getUploadUrls`, `${failedKeys} fail - ${result.reason}`);
+      }
+    });
+    return { data: { fileUrls, failedKeys } };
+  }
+
+  fastify.decorate('listBuckets', async function (req, reply) {
+    try {
+      const output = {};
+      const buckets = (await s3Client.send(new ListBucketsCommand({})))?.Buckets || [];
+      const listDistributionsCmd = new ListDistributionsCommand({});
+      const distributions = [];
+      const s3BucketPattern = /(.+)\.s3-website-ap-northeast-1\.amazonaws\.com/;
+
+      do {
+        const resp = await cloudfrontClient.send(listDistributionsCmd);
+        const distribList = resp?.DistributionList?.Items;
+
+        if (distribList?.length) {
+          distribList.forEach((distrib) => distributions.push(distrib));
+        }
+        listDistributionsCmd.input.Marker = resp?.DistributionList?.NextMarker;
+      } while (listDistributionsCmd.input.Marker);
+
+      const bucketNames = [];
+
+      buckets.forEach((bucket) => {
+        bucketNames.push(bucket.Name);
+        output[bucket.Name] = null;
+      });
+      distributions.forEach((distrib) => {
+        const originNameList = distrib.Origins.Items.map((i) => {
+          const match = i.DomainName.match(s3BucketPattern);
+          return match ? match[1] : null;
+        });
+
+        for (let i = 0; i < originNameList.length; i++) {
+          if (bucketNames.includes(originNameList[i])) {
+            output[originNameList[i]] = {
+              cdnID: distrib.Id,
+              domain: distrib.Aliases.Items[i],
+            };
+            break;
+          }
+        }
+      });
+      return reply.send({ data: output });
+    } catch (err) {
+      fastify.logStat('error', `${req.user.username}->listBuckets`, `${err.name}: ${err.message}`);
+      return reply.code(500).send();
+    }
+  });
+
+  fastify.decorate('getObjects', async function (req, reply) {
+    let result;
+
+    switch (req.query.type) {
+      case 'structure':
+        result = await listAllObjs(req);
+        if (result) return reply.send(result);
+        if (result === undefined) return reply.code(404).send({ message: 'The specified bucket does not exist' });
+        return reply.code(500).send({ message: 'Fail to get the bucket\'s structure' });
+      case 'file':
+        return reply.send(await downloadFiles(req, reply));
+      case 'versions':
+        result = await getObjVersion(req);
+        if (result) return reply.send(result);
+        if (result === undefined) return reply.code(404).send({ message: 'The specified bucket does not exist' });
+        return reply.code(500).send({ message: 'Fail to get object\'s version' });
+      default:
+        return reply.code(400).send();
+    }
+  });
+
+  fastify.decorate('createObjects', async function (req, reply) {
+    if (req.query.type === 'folder') {
+      const result = await createFolder(req);
+
+      if (result) return reply.send(result);
+      else if (result === undefined) return reply.code(404).send({ message: 'The specified bucket does not exist' });
+      return reply.code(500).send({ message: 'Fail to create folder' });
+    }
+    return reply.send(await getUploadUrls(req));
+  });
+
+  fastify.decorate('deleteObjects', async function (req, reply) {
+    const Bucket = req.params.bucket;
+    const user = req.user.username;
+
+    try {
+      const result = await s3Client.send(new DeleteObjectsCommand({
+        Bucket,
+        Delete: { Objects: req.body },
+      }));
+      const success = result?.Deleted?.map((delInfo) => delInfo.Key) || [];
+      const failure = result?.Errors?.map((err) => err.Key) || [];
+
+      await Promise.allSettled(success.map((key) => fastify.recordOper(Bucket, key, 'delete', user)));
+      fastify.logStat('info', `${user}->deleteObjects`, `Fail deletions: ${failure.join(',') || 'null'}`);
+      return reply.send({ data: { success, failure } });
+    } catch (err) {
+      fastify.logStat('error', `${user}->deleteObjects`, `${err.name}: ${err.message}`);
+      if (err.name === 'NoSuchBucket') return reply.code(404).send({ message: err.message });
+      return reply.code(500).send();
+    }
+  });
+
+  fastify.decorate('modifyObjInfo', async function (req, reply) {
+    const Bucket = req.params.bucket;
+    const user = req.user.username;
+    const { items, isPublic } = req.body;
+
+    const failure = [];
+    const successCopy = [];
+    const copyResults = await Promise.allSettled(
+      items.map((item) => s3Client.send(new CopyObjectCommand({
+        Bucket,
+        CopySource: encodeURI(`${Bucket}/${item.oriKey}`),
+        Key: item.newKey,
+        ...(isPublic && { ACL: 'public-read' }),
+      })))
+    );
+
+    copyResults.forEach((result, index) => {
+      if (result.status === 'fulfilled') successCopy.push(items[index].oriKey);
+      else failure.push(items[index].oriKey);
+    });
+
+    try {
+      const deleteResult = await s3Client.send(new DeleteObjectsCommand({
+        Bucket,
+        Delete: { Objects: successCopy.map((Key) => ({ Key })) }
+      }));
+      const success = deleteResult?.Deleted?.map((delInfo) => delInfo.Key) || [];
+      const failDelKeys = deleteResult?.Errors?.map((err) => err.Key) || [];
+
+      for (let i = 0; i < failDelKeys.length; i++) failure.push(failDelKeys[i]);
+      await Promise.allSettled(success.map((key) => fastify.recordOper(Bucket, key, 'modify', user)));
+      fastify.logStat('info', `${user}->modifyObjInfo`, `Failing Modification list: ${failure.join(',') || 'null'}`);
+      return reply.send({ data: { success, failure } });
+    } catch (err) {
+      fastify.logStat('error', `${user}->modifyObjInfo`, `${err.name}: ${err.message}`);
+      if (err.name === 'NoSuchBucket') return reply.code(404).send({ message: err.message });
+      return reply.code(500).send();
+    }
+  });
+
+  fastify.decorate('changeObjVer', async function (req, reply) {
+    const Bucket = req.params.bucket;
+    const { verId, key, isPublic } = req.query;
+    const user = req.user.username;
+
+    try {
+      await s3Client.send(new CopyObjectCommand({
+        Bucket,
+        CopySource: encodeURI(`${Bucket}/${key}?versionId=${verId}`),
+        Key: key,
+        ...(Boolean(isPublic) && { ACL: 'public-read' }),
+      }));
+      await s3Client.send(new DeleteObjectCommand({
+        Bucket,
+        Key: key,
+        VersionId: verId,
+      }));
+      await fastify.recordOper(Bucket, key, 'switch', user);
+      fastify.logStat('info', `${user}->changeObjVer`, `Success to switch the version of the object (${key})`);
+      return reply.send({ data: null });
+    } catch (err) {
+      fastify.logStat('error', `${user}->changeObjVer`, `${err.name}: ${err.message}`);
+      if (err.name === 'NoSuchBucket' || err.name === 'NoSuchVersion') {
+        return reply.code(404).send({ message: err.message });
+      }
+      return reply.code(500).send();
+    }
+  });
+
+  fastify.decorate('restoreDelObj', async function (req, reply) {
+    const Bucket = req.params.bucket;
+    const user = req.user.username;
+    const { reqList } = req.body;
+
+    const failure = [];
+    const curDelList = [];
+
+    try {
+      const getVersionResults = await Promise.allSettled(
+        reqList.map((objKey) => s3Client.send(
+          new ListObjectVersionsCommand({ Bucket, Prefix: objKey })
+        ))
+      );
+
+      getVersionResults.forEach((result, index) => {
+        const curObjKey = reqList[index];
+
+        if (result.status === 'fulfilled') {
+          const delVersions = result.value.DeleteMarkers;
+
+          if (!result.value.Versions) failure.push({ key: curObjKey, reason: 'The object has never existed' });
+          else if (!delVersions || !delVersions[0].IsLatest) {
+            failure.push({ key: curObjKey, reason: 'The object is still alive' });
+          } else {
+            curDelList.push({ Key: curObjKey, VersionId: delVersions[0].VersionId });
+          }
+        } else {
+          fastify.logStat('error', `${user}->restoreDelObj`, `${curObjKey} fail - ${result.reason}`);
+          failure.push({ key: curObjKey, reason: result.reason.message });
+        }
+      });
+
+      const deleteResult = curDelList.length
+        ? await s3Client.send(new DeleteObjectsCommand({
+          Bucket,
+          Delete: { Objects: curDelList },
+        })) : {};
+      const success = deleteResult?.Deleted?.map((delInfo) => delInfo.Key) || [];
+
+      deleteResult?.Errors?.forEach((err) => {
+        failure.push({ key: err.Key, reason: 'Internal server error' });
+        fastify.logStat('error', `${user}->restoreDelObj`, err.Message);
+      });
+
+      await Promise.allSettled(success.map((key) => fastify.recordOper(Bucket, key, 'restore', user)));
+      return reply.send({ data: { success, failure } });
+    } catch (err) {
+      fastify.logStat('error', `${user}->restoreDelObj`, `${err.name}: ${err.message}`);
+      return reply.code(500).send();
+    }
+  });
+
+  fastify.decorate('refreshCDN', async function (req, reply) {
+    const user = req.user.username;
+    const { DistributionId, Items } = req.body;
+
+    try {
+      await cloudfrontClient.send(new CreateInvalidationCommand({
+        DistributionId,
+        InvalidationBatch: {
+          CallerReference: Date.now().toString(),
+          Paths: { Quantity: Items.length, Items },
+        },
+      }));
+      await fastify.logStat('info', `${user}->refreshCDN`, `Success to refresh cdn (id: ${DistributionId})`);
+      return reply.send({ data: null });
+    } catch (err) {
+      fastify.logStat('error', `${user}->refreshCDN`, `${err.name}: ${err.message}`);
+      return reply.code(500).send();
+    }
+  });
+
+}, {
+  name: 's3Handler',
+  dependencies: ['s3Recorder', 'logHandler'],
+});
--- api_server/routes/s3Buckets/index.mjs
+++ api_server/routes/s3Buckets/index.mjs
@@ -1,5 +1,220 @@
+import S from 'fluent-json-schema';
+
 export default async function (fastify, _opts) {
-  fastify.get('/', async function (req, reply) {
-    reply.send('s3Bucket route');
-  });
+  const respInvalidReq = fastify.clientErrResponse();
+  const respUnpermitReq = fastify.clientErrResponse(
+    S.object().description('Insufficient permissions'),
+  );
+
+  const optsGetBuckets = {
+    schema: {
+      summary: 'List all S3 objects',
+      description: 'Get informations about S3 objectss',
+      tags: ['S3 objects'],
+      security: [{ jwt: [] }],
+      response: {
+        200: S.object()
+          .description('Success to get informations of all buckets')
+          .prop('data', S.object().additionalProperties(true)),
+        403: respUnpermitReq,
+        500: fastify.svrErrResponse,
+      },
+    },
+    onRequest: [fastify.verifyToken, fastify.checkAdmin],
+  };
+
+  const optsGetObjs = {
+    schema: {
+      summary: 'Get S3 objects',
+      description: 'Get objects within the specific bucket',
+      tags: ['S3 objects'],
+      security: [{ jwt: [] }],
+      params: S.object().prop('bucket', fastify.schemaNotEmpty.required()),
+      querystring: S.object()
+        .allOf([
+          S.object().prop('type', S.string().enum(['structure', 'file', 'versions']).required()),
+          S.ifThen(
+            S.object().prop('type', S.string().const('file')),
+            S.object().prop('reqList', fastify.schemaNotEmpty.required())
+          ),
+          S.ifThen(
+            S.object().prop('type', S.string().const('versions')),
+            S.object().prop('objKey', fastify.schemaNotEmpty.required())
+          ),
+        ]),
+      response: {
+        200: S.object()
+          .description('Success to get objects')
+          .prop('data', S.object().additionalProperties(true)),
+        400: respInvalidReq,
+        403: respUnpermitReq,
+        404: respInvalidReq,
+        500: fastify.svrErrResponse,
+      },
+    },
+    onRequest: [fastify.verifyToken, fastify.checkPermission],
+  };
+
+  const optsCreateObjs = {
+    schema: {
+      summary: 'Create new object',
+      description: 'Create new folder or upload files to specific bucket',
+      tags: ['S3 objects'],
+      security: [{ jwt: [] }],
+      params: S.object().prop('bucket', fastify.schemaNotEmpty.required()),
+      querystring: S.object().prop('type', S.string().enum(['folder', 'file']).required()),
+      body: S.object()
+        .prop('objKeys', S.array().items(fastify.schemaNotEmpty).minItems(1).required())
+        .prop('isPublic', S.boolean()),
+      response: {
+        200: S.object()
+          .description('Success to create object')
+          .prop('data', S.object()
+            .prop('fileUrls', S.array().items(S.object().additionalProperties(true)))
+            .prop('failedKeys', S.array().items(S.string()))
+          ),
+        400: respInvalidReq,
+        403: respUnpermitReq,
+        404: respInvalidReq,
+        500: fastify.svrErrResponse,
+      },
+    },
+    onRequest: [fastify.verifyToken, fastify.checkPermission],
+  };
+
+  const optsDeleteObjs = {
+    schema: {
+      summary: 'Delete objects',
+      description: 'Delete files/folders within specific bucket',
+      tags: ['S3 objects'],
+      security: [{ jwt: [] }],
+      params: S.object().prop('bucket', fastify.schemaNotEmpty.required()),
+      body: S.array().items(S.object().prop('Key', fastify.schemaNotEmpty.required())).minItems(1),
+      response: {
+        200: S.object()
+          .description('Success to delete specific objects in the bucket')
+          .prop('data', S.object()
+            .prop('success', S.array().items(S.string()))
+            .prop('failure', S.array().items(S.string()))
+          ),
+        400: respInvalidReq,
+        403: respUnpermitReq,
+        404: respInvalidReq,
+        500: fastify.svrErrResponse,
+      },
+    },
+    onRequest: [fastify.verifyToken, fastify.checkPermission],
+  };
+
+  const optsModifyObjs = {
+    schema: {
+      summary: 'Update objects',
+      description: 'Modify objects\' name/directory in the bucket',
+      tags: ['S3 objects'],
+      security: [{ jwt: [] }],
+      params: S.object().prop('bucket', fastify.schemaNotEmpty.required()),
+      body: S.object()
+        .prop('items', S.array()
+          .items(S.object()
+            .prop('oriKey', fastify.schemaNotEmpty.required())
+            .prop('newKey', fastify.schemaNotEmpty.required())
+          ).minItems(1).required())
+        .prop('isPublic', S.boolean().required()),
+      response: {
+        200: S.object()
+          .description('Success to modify objects')
+          .prop('data', S.object()
+            .prop('success', S.array().items(S.string()))
+            .prop('failure', S.array().items(S.string()))),
+        400: respInvalidReq,
+        403: respUnpermitReq,
+        404: respInvalidReq,
+        500: fastify.svrErrResponse,
+      },
+    },
+    onRequest: [fastify.verifyToken, fastify.checkPermission],
+  };
+
+  const optsChangeVersion = {
+    schema: {
+      summary: 'Update object',
+      description: 'Switch the verison of the object',
+      tags: ['S3 objects'],
+      security: [{ jwt: [] }],
+      params: S.object().prop('bucket', fastify.schemaNotEmpty.required()),
+      querystring: S.object()
+        .prop('verId', fastify.schemaNotEmpty.required())
+        .prop('key', fastify.schemaNotEmpty.required())
+        .prop('isPublic', S.boolean().enum([true, false])),
+      response: {
+        200: S.object()
+          .description('Success to change object\'s version')
+          .prop('data', S.null()),
+        400: respInvalidReq,
+        403: respUnpermitReq,
+        404: respInvalidReq,
+        500: fastify.svrErrResponse,
+      }
+    },
+    onRequest: [fastify.verifyToken, fastify.checkPermission],
+  };
+
+  const optsRestoreDelObj = {
+    schema: {
+      summary: 'Restore deleted objects',
+      description: 'Restore deleted objects',
+      tags: ['S3 objects'],
+      security: [{ jwt: [] }],
+      params: S.object().prop('bucket', fastify.schemaNotEmpty.required()),
+      body: S.object().prop('reqList', S.array().items(S.string()).required()),
+      response: {
+        200: S.object()
+          .description('Finish to process the restore request')
+          .prop('data', S.object()
+            .prop('success', S.array().items(S.string()))
+            .prop('failure', S.array().items(S.object()
+              .prop('key', S.string())
+              .prop('reason', S.string())
+            ))
+          ),
+        400: respInvalidReq,
+        403: respUnpermitReq,
+        500: fastify.svrErrResponse,
+      },
+    },
+    onRequest: [fastify.verifyToken, fastify.checkPermission],
+  };
+
+  const optsRefreshCDN = {
+    schema: {
+      summary: 'Refresh cloudfront',
+      description: 'Refresh the distributions of the bucket',
+      tags: ['S3 objects'],
+      security: [{ jwt: [] }],
+      body: S.object()
+        .prop('DistributionId', fastify.schemaNotEmpty.required())
+        .prop('Items', S.array().items(S.string()).minItems(1).required()),
+      response: {
+        200: S.object()
+          .description('Success to refresh the distributions')
+          .prop('data', S.null()),
+        400: respInvalidReq,
+        403: respUnpermitReq,
+        500: fastify.svrErrResponse,
+      }
+    },
+    onRequest: [fastify.verifyToken, fastify.checkPermission],
+  };
+
+  fastify.get('/infos', optsGetBuckets, fastify.listBuckets);
+
+  fastify.get('/:bucket/objects', optsGetObjs, fastify.getObjects);
+  fastify.put('/:bucket/objects', optsCreateObjs, fastify.createObjects);
+  fastify.delete('/:bucket/objects', optsDeleteObjs, fastify.deleteObjects);
+  fastify.patch('/:bucket/objects/infos', optsModifyObjs, fastify.modifyObjInfo);
+  fastify.patch('/:bucket/object/version', optsChangeVersion, fastify.changeObjVer);
+  fastify.patch('/:bucket/objects/existence', optsRestoreDelObj, fastify.restoreDelObj);
+
+  fastify.patch('/distributions', optsRefreshCDN, fastify.refreshCDN);
+
 };
