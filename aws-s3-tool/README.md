7c02708d56ed19f6113f8f2160c3b6120d74beb2
Allen <allen.chen@happytomato.com.tw>
2024-01-03 09:09:03 +0800

    [aws-s3-tool] proj init

---
+++ aws-s3-tool/README.md
@@ -0,0 +1,33 @@
+# aws-s3-tool (aws-s3-tool)
+
+A tool for manipulating objects in AWS S3 service
+
+## Install the dependencies
+```bash
+yarn
+# or
+npm install
+```
+
+### Start the app in development mode (hot-code reloading, error reporting, etc.)
+```bash
+quasar dev
+```
+
+
+### Lint the files
+```bash
+yarn lint
+# or
+npm run lint
+```
+
+
+
+### Build the app for production
+```bash
+quasar build
+```
+
+### Customize the configuration
+See [Configuring quasar.config.js](https://v2.quasar.dev/quasar-cli-vite/quasar-config-js).
---
+++ aws-s3-tool/src/layouts/MainLayout.vue
@@ -0,0 +1,25 @@
+<template>
+  <q-layout view="hHh LpR fFf">
+    <q-header elevated class="bg-blue-2">
+      <q-bar class="glossy q-py-md">
+        <q-icon class="q-mr-md" name="fa-solid fa-database" />
+      </q-bar>
+    </q-header>
+
+    <q-page-container>
+      <q-page :style-fn="tweakPageH" class="bg-light-blue-1" style="overflow: hidden">
+
+      </q-page>
+    </q-page-container>
+
+    <q-footer elevated class="bg-blue-2">
+
+    </q-footer>
+  </q-layout>
+</template>
+
+<script setup>
+
+const tweakPageH = (offset) => ({ height: offset ? `calc(100vh - ${offset}px)` : '100vh' });
+
+</script>
