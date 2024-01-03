8d7b614a79675b2716bc6e3842a793d68ec5ba15
Allen <allen.chen@happytomato.com.tw>
2024-01-03 15:15:36 +0800

    [aws-s3-tool] feat: define app variables

---
+++ aws-s3-tool/src/stores/appStatus.js
@@ -0,0 +1,48 @@
+import { defineStore } from 'pinia';
+import { Platform } from 'quasar';
+import { ref } from 'vue';
+
+export const useAppStatusStore = defineStore('appStatus', () => (
+  {
+    reserveWords: process.env.RESERVE_WORDS.split(','),
+    isMobile: Platform.is.mobile,
+
+    contentScrollArea: ref(null),
+
+    dragSelect: ref(null),
+    selections: ref([]),
+    onlySelectFile: ref(false),
+    multiChoose: ref(false),
+    curSelectUrl: ref(''),
+    previewImgUrl: ref(''),
+    previewDocUrl: ref(''),
+    previewYaml: ref(''),
+
+    cutStorage: ref([]),
+    renameObjId: ref(''),
+    preName: ref(''),
+    tempName: ref(''),
+    reqList: ref(null),
+    refreshList: ref([]),
+
+    isProcessing: ref(false),
+    isUploading: ref(false),
+    uploadLimit: 50,
+    onModifying: ref(false),
+    onCreatingFolder: ref(false),
+    onCutting: ref(false),
+
+    preStep: ref([]),
+    nextStep: ref([]),
+
+    isOperObj: ref(false),
+
+    queryString: ref(''),
+    queryResult: ref([]),
+
+    operHist: ref([]),
+
+    curObjVerList: ref([]),
+    newObjVerIndex: ref(null),
+  }
+));
